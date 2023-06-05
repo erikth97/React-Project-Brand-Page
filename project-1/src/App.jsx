3cb68b44452bd8f79a0c672bef537c9dd711c836
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-06-05 14:35:39 -0600

    navbar

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -3,19 +3,7 @@ import "./App.css"
 const App = () => {
    return (
    <div>
-    <nav>
-        <div className="logo">
-          <img src="../public/images/logo.png" alt="densologo"></img>
-        </div>
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-        
-        <button>Login</button>
-    </nav>
+
    </div> 
    );
 };
---
+++ project-1/src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+    return(
+        <nav>
+        <div className="logo">
+        <img src="../public/images/logo.png" alt="densologo"></img>
+        </div>
+        <ul>
+            <li href="#">Menu</li>
+            <li href="#">Location</li>
+            <li href="#">About</li>
+            <li href="#">Contact</li>
+        </ul>
+        
+        <button>Login</button>
+    </nav>
+    );
+};
+
+export default Navigation
