3aa85f6820c034c55298749f740d022f04c8d016
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-06-05 14:14:00 -0600

    navbar displayflex

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,22 +1,23 @@
+import "./App.css"
+
 const App = () => {
    return (
    <div>
     <nav>
-        <div>
+        <div className="logo">
           <img src="../public/images/logo.png" alt="densologo"></img>
         </div>
-
         <ul>
           <li href="#">Menu</li>
           <li href="#">Location</li>
           <li href="#">About</li>
           <li href="#">Contact</li>
         </ul>
-
+        
         <button>Login</button>
     </nav>
    </div> 
-   )
+   );
 };
 
 export default App;
