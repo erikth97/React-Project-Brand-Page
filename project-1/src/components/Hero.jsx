44b353995f4474eae4c9bd147df09909a193fe2f
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-06-05 15:03:22 -0600

    body

---
+++ project-1/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+const HeroSection = () => {
+    return (
+        <main className="hero">
+          <div>
+            <h1>YOU</h1>
+            <p></p>
+          </div>
+        </main>
+
+    );
+};
+
+export default HeroSection;
