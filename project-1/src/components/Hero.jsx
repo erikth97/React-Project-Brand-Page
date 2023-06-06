63a8ae9bd2c43719e20388afd5611ffc96a7ab6a
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-06-06 11:58:51 -0600

    body

--- project-1/src/components/Hero.jsx
+++ project-1/src/components/Hero.jsx
@@ -1,12 +1,16 @@
 const HeroSection = () => {
     return (
         <main className="hero">
-          <div>
-            <h1>YOU</h1>
-            <p></p>
+          <div className="hero-content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+             <p>
+            YOUR FEET DESERVE THE BEST AND HERE TO HELP YOU WITH OUR SHOES. YOUR FEET DESERVE 
+            THE BEST AND WHERE HERE TO HELP YOU WHIT OUR 
+            SHOES. 
+            </p>
           </div>
+          <div className="hero-image"></div>
         </main>
-
     );
 };
 
